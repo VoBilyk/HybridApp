160267d76262d40a0308bb438e68c98cb007a00e
Volodymyr <volodymyr_bilyk@outlook.com>
2018-10-03 18:47:23 +0300

    added web config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home-component/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent, },
   { path: 'helloapp', loadChildren: './modules/hello-app.module#HelloAppModule', pathMatch: 'full' },
   { path: 'numbers', loadChildren: './modules/numbers-app.module#NumbersJsAppModule', pathMatch: 'full' },
